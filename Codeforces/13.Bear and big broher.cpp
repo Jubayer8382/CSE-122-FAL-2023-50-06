24f6445bc218dfd07c8d40a10034e40a24e43472
Jubayer Ahmed <122223944+Jubayer8382@users.noreply.github.com>
2023-06-17 22:39:20 +0600

    Add files via upload

---
+++ Codeforces/13.Bear and big broher.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,i;
+    cin>>l>>b;
+    for(i=1;;i++)
+    {
+        l*=3;
+        b*=2;
+    if(l>b)break;
+    }
+    cout<<i;
+}
