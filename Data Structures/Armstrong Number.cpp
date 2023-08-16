3e81cc2cd921cc7f0f5564fb3d083d13c752bc93
Jubayer Ahmed <122223944+Jubayer8382@users.noreply.github.com>
2023-08-16 14:11:49 +0600

    Add files via upload

---
+++ Data Structures/Armstrong Number.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int ord1, ord2, ord3, total_sum;
+
+    cout << "All the Armstrong numbers between 1 to 1000 : ";
+
+
+    for (int num = 1; num <= 1000; ++num)
+    {
+        if (num <= 9)
+        {
+            cout << num << " ";
+        }
+        else
+        {
+            ord1 = num % 10;
+            ord2 = (num % 100 - ord1) / 10;
+            ord3 = (num % 1000 - ord2) / 100;
+
+            total_sum = ((ord1 * ord1 * ord1) +
+                         (ord2 * ord2 * ord2) +
+                         (ord3 * ord3 * ord3));
+            if (total_sum == num)
+            {
+                cout << num << " ";
+            }
+            return 0;
+        }
+
+    }
+}
