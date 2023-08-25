f25f489fbda62db123aeaab4d409b7579a36f09e
Jubayer Ahmed <122223944+Jubayer8382@users.noreply.github.com>
2023-08-25 22:45:50 +0600

    Add files via upload

---
+++ Data Structures/substring 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+
+    string s1 = "jubayer";
+
+
+
+    string r = s1.substr(3, 2);
+
+
+    cout << "String is: " << r;
+
+    return 0;
+}
